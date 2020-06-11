3d0cfb120cdbb6a326bad8a25f437acc4f23008b
Jared Manwaring <jaredmanwaring@me.com>
2020-06-11 10:10:07 -0600

    latest commit

--- ecommerce-site/src/Cart.js
+++ ecommerce-site/src/Cart.js
@@ -1,13 +1,15 @@
 import React, { useContext } from "react";
+import StripeCheckout from "react-stripe-checkout"
 import { CartContext } from "./CartContext";
 
 function Cart() {
   const [cart, setCart] = useContext(CartContext);
+  const totalPrice = cart.reduce((acc, curr) => acc + curr.price, + '')
   return (
     <div>
-      <span>Items in Cart{cart.length}</span>
+      <span>Items in Cart: {cart.length}</span>
       <br />
-      <span>total price: 0</span>
+      <span>{`$${totalPrice}`}</span>
     </div>
   );
 }
--- ecommerce-site/src/Map.js
+++ ecommerce-site/src/Map.js
@@ -6,18 +6,20 @@ export const Map = (props) => {
 
   const addToCart = () => {
     const tShirt = { name: props.name, price: props.price, image: props.image };
-    setCart((current) => [...current, tShirt]);
+    setCart(current => [...current, tShirt]);
     console.log("Something is happening");
   };
 
   return (
-    <div>
-      <h1>{props.name}</h1>
-      <img src={props.image} alt="" />
-      <h4>{props.price}</h4>
+    <div className="container">
+      <div className="item">
+        <h1>{props.name}</h1>
+        <img src={props.image} alt="" />
+        <h4>{props.price}</h4>
 
-      <button onClick={addToCart}>Add to Cart</button>
-      <hr />
+        <button onClick={addToCart}>Add to Cart</button>
+        <hr />
+      </div>
     </div>
   );
 };

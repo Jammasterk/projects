9193317ec0b2b598c1e72449e42cd788c5bf69cd
Jared Manwaring <jaredmanwaring@me.com>
2020-06-11 20:43:26 -0600

    added portfolio project

---
+++ herb-store/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Splash from "./components/Splash"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Splash/>
+    </div>
+  );
+}
+
+export default App;
---
+++ herb-store/src/components/Splash.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+
+export class Splash extends Component {
+    render() {
+        return (
+            <div className="container">
+            <div className="sectionOne">
+                <h1>HIIT Training</h1>
+                <p>Programs</p>
+            </div>
+            <div className="sectionTwo">
+                <h1>Weight Training</h1>
+                <p>Trainers</p>
+            </div>
+            <div className="sectionThree">
+                <h1>Endurance</h1>
+                <p>Equipment</p>
+            </div>
+            <div className="sectionFour">
+                <h1>Flexibility</h1>
+                <p>Equipment</p>
+            </div>
+            <div className="sectionFive">
+                <h1>Yoga</h1>
+                <p>Instructors</p>
+            </div>
+            <div className="sectionSix">
+                <h1>Pilates</h1>
+                <p>Classes</p>
+            </div>
+            <div className="sectionSeven">
+                <h1>Distance Running</h1>
+                <p>Running Gear</p>
+            </div>
+            <div className="sectionEight">
+                <h1>Athletics</h1>
+                <p>Training Equipment</p>
+            </div>
+            <div className="sectionNine">
+                <h1>Mobility</h1>
+                <p>Equipment</p>
+            </div>            
+        </div>
+        )
+    }
+}
+
+export default Splash
+

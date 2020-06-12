be5cc8cd24ee2c714bcaf1893c1b23e28bf499de
Jared Manwaring <jaredmanwaring@me.com>
2020-06-12 00:18:07 -0600

    more work

---
+++ herb-store/src/components/Packages.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Packages extends Component {
+    render() {
+        return (
+            <div>
+                Packages
+            </div>
+        )
+    }
+}
+
+export default Packages
--- herb-store/src/components/Splash.js
+++ herb-store/src/components/Splash.js
@@ -1,47 +1,46 @@
 import React, { Component } from 'react'
+import {Redirect, Link, Switch, Route} from "react-router-dom"
+import Packages from "./Packages"
+import Contact from "./Contact"
 
 export class Splash extends Component {
     render() {
         return (
-            <div className="container">
+          <div className="container">
             <div className="sectionOne">
-                <h1>HIIT Training</h1>
-                <p>Programs</p>
+              <h1 className="header">Jackson Photography</h1>
+              <p className="sub-header">
+                Artist / Designer / Photographer / Videographer
+              </p>
             </div>
-            <div className="sectionTwo">
-                <h1>Weight Training</h1>
-                <p>Trainers</p>
-            </div>
-            <div className="sectionThree">
-                <h1>Endurance</h1>
-                <p>Equipment</p>
-            </div>
-            <div className="sectionFour">
-                <h1>Flexibility</h1>
-                <p>Equipment</p>
-            </div>
+            <div className="sectionTwo"></div>
+            <div className="sectionThree"></div>
+            <div className="sectionFour"></div>
             <div className="sectionFive">
-                <h1>Yoga</h1>
-                <p>Instructors</p>
+              <Link className="links" to="/">
+                Packages
+              </Link>
+              <Switch>
+                <Route path="/Packages">
+                  <Packages />
+                </Route>
+              </Switch>
             </div>
-            <div className="sectionSix">
-                <h1>Pilates</h1>
-                <p>Classes</p>
-            </div>
+            <div className="sectionSix"></div>
             <div className="sectionSeven">
-                <h1>Distance Running</h1>
-                <p>Running Gear</p>
+              <Link className="links" to="/Contact">
+                Contact Me
+              </Link>
+              <Switch>
+                <Route path="/Contact">
+                  <Contact />
+                </Route>
+              </Switch>
             </div>
-            <div className="sectionEight">
-                <h1>Athletics</h1>
-                <p>Training Equipment</p>
-            </div>
-            <div className="sectionNine">
-                <h1>Mobility</h1>
-                <p>Equipment</p>
-            </div>            
-        </div>
-        )
+            <div className="sectionEight"></div>
+            <div className="sectionNine"></div>
+          </div>
+        );
     }
 }
 

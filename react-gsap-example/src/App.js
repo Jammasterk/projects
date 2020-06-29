109b340d6cc82f24f6fdcaed07af362a12c6d611
Jared Manwaring <jaredmanwaring@me.com>
2020-06-29 09:34:48 -0600

    new push

---
+++ react-gsap-example/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import ImageFade from "./components/ImageFade.js"
+import './App.css';
+import CircleAnimation from './components/CircleAnimations';
+
+
+function App() {
+  return (
+    <div className="App">
+     <CircleAnimation/>
+     {/* <ImageFade /> */}
+    </div>
+  );
+}
+
+export default App;

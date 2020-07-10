c9e4cdfb837c704ecad8b9ceeac2af7b8ee2f45b
Jared Manwaring <jaredmanwaring@me.com>
2020-07-09 19:21:15 -0600

    working on desktop site

---
+++ tweener-clone/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Home from "./components/Home"
+import "./index.css"
+
+export class App extends Component {
+    render() {
+        return (
+            <div>
+                <Home />
+            </div>
+        )
+    }
+}
+
+export default App

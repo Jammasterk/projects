146b317fb0b428b598e8b39e4df6f9b8d5c49988
Jared Manwaring <jaredmanwaring@me.com>
2020-08-27 13:16:37 -0600

    added more functionality

--- multiuser-blog/client/src/components/Blog.js
+++ multiuser-blog/client/src/components/Blog.js
@@ -1,6 +1,16 @@
 import React, { useState } from "react";
 import BlogForm from "./BlogForm";
-import { MDBJumbotron, MDBContainer, MDBRow, MDBCol, MDBBadge } from "mdbreact";
+import {
+  MDBJumbotron,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBBadge,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+} from "mdbreact";
 import styled from "styled-components";
 import moment from "moment";
 
@@ -8,8 +18,44 @@ const Wrapper = styled.div`
   .tag {
     padding: 3px 6px !important;
   }
-  .lead{
-    margin-top: 2em
+  .lead {
+    margin-top: 2em;
+    width: 100vw;
+    padding: 2em;
+  }
+  .dark {
+    color: #000 !important;
+    background: none !important;
+  }
+
+  .preview{
+    margin-top: 1.5em
+  }
+  @media only screen and (max-width: 600px) {
+      .block{
+        display: block !important
+      };
+      img{
+        /* width: 100%; */
+        margin: 0 auto
+      }
+      .text{
+        max-width: 100%
+      }
+      .text-container{
+        width: 100%;
+        
+        
+      }
+      .med-width{
+        width: 100% !important
+      }
+      .btn{
+        margin-top: 4em;
+        margin-left: 0;
+        margin-right: 0;
+        margin-bottom: 0
+      }
   }
 `;
 
@@ -17,6 +63,7 @@ const Wrapper = styled.div`
 const Blog = (props) => {
   const date = moment().format("MMM Do YY");
   const { title, author, image, content, tags, _id } = props;
+  const abreviated = content.slice(0, 150)
   const [editToggle, setEditToggle] = useState(false);
   return (
     <div>
@@ -24,27 +71,50 @@ const Blog = (props) => {
         <>
           <Wrapper>
             <MDBContainer className="mt-5 text-center">
-              <MDBRow>
-                <MDBCol>
-                  <MDBJumbotron>
-                    <h4>{title}</h4>
-                    <h6>Author: {author}</h6>
-                    <small>Date published: {date}</small>
-                    <img src={image} alt="" />
-                    <p className="lead">{content}</p>
-                    <hr className="my-2" />
-                    <MDBBadge color="danger" className="mb-2 px-3 py-2">
-                      {tags}
-                    </MDBBadge>
-                    <br />
-                    <button
-                      onClick={() => setEditToggle((prevToggle) => !prevToggle)}
-                    >
-                      Edit
-                    </button>
-                  </MDBJumbotron>
-                </MDBCol>
-              </MDBRow>
+              <MDBJumbotron>
+                <MDBRow className="w-100 block">
+                  <MDBCol className="med-width w-50">
+                    <div className="text-container">
+                      <div className="text">
+                        <h4>{title}</h4>
+                        <h6>Author: {author}</h6>
+                        <small>Date published: {date}</small>
+                      </div>
+                      <img src={image} alt="" />
+                    </div>
+                  </MDBCol>
+                  <MDBCol>
+                    <p className="preview">{abreviated}...</p>
+                    <MDBDropdown dropright className="w-100">
+                      <MDBDropdownToggle
+                        caret
+                        outline
+                        color="dark"
+                        className="w-100 dark"
+                      >
+                        View Article
+                      </MDBDropdownToggle>
+                      <MDBDropdownMenu basic>
+                        <img src={image} alt=""/>
+                        <h4>{title}</h4>
+                        <p className="lead">{content}</p>
+                        <hr className="my-2" />
+                        <MDBBadge color="danger" className="mb-2 px-3 py-2">
+                          {tags}
+                        </MDBBadge>
+                        <br />
+                        <button
+                          onClick={() =>
+                            setEditToggle((prevToggle) => !prevToggle)
+                          }
+                        >
+                          Edit
+                        </button>
+                      </MDBDropdownMenu>
+                    </MDBDropdown>
+                  </MDBCol>
+                </MDBRow>
+              </MDBJumbotron>
             </MDBContainer>
           </Wrapper>
         </>

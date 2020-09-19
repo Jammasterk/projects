a695866b463a1bbf1989f08c820ed207d73f3b85
Jared Manwaring <jaredmanwaring@me.com>
2020-09-19 10:09:48 -0600

    more note page styling

--- note-taking-app/client/src/components/Note.js
+++ note-taking-app/client/src/components/Note.js
@@ -65,6 +65,19 @@ const Note = (props) => {
     e.preventDefault(setShow(!show))
   }
 
+  const upcaret = (
+    <svg
+      width="1em"
+      height="1em"
+      viewBox="0 0 16 16"
+      class="bi bi-caret-down-fill"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M7.247 11.14L2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+    </svg>
+  );
+
   const {title, note, date} = props
     return (
       <Wrapper>
@@ -74,7 +87,7 @@ const Note = (props) => {
             <h6>{date}</h6>
           </div>
           <div className="">
-            <a onClick={handleClick}>Show note </a>
+            <a onClick={handleClick}>{show ? "Hide" : "Show"} note</a>
             <p style={show ? { display: "block" } : { display: "none" }}>
               {note}
             </p>

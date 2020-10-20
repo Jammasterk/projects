e53ed37d411e52f6969106060c1e84963c409b79
Jared Manwaring <jaredmanwaring@me.com>
2020-10-20 14:57:35 -0600

    first deploy

--- contacts/server.js
+++ contacts/server.js
@@ -5,20 +5,23 @@ const morgan = require('morgan')
 const expressJwt = require('express-jwt')
 require('dotenv').config()
 const PORT = process.env.PORT || 7500
+const path = require("path");
 
 process.env.SECRET 
 
 app.use(express.json())
 app.use(morgan('dev'))
 
-mongoose.connect('mongodb://localhost:27017/contact-authentication', {
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/contact-authentication",
+  {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
-},
-    ()=>console.log('Connected to the database')
-)
+    useFindAndModify: false,
+  },
+  () => console.log("Connected to the database")
+);
 
 app.use("/auth", require("./routes/authRouter.js"));
 app.use(
@@ -34,6 +37,15 @@ app.use((err, req, res, next) => {
     }
     return res.send({errMsg: err.message})
 })
+
+// ... other app.use middleware setups
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`The server is running on port ${PORT}`)

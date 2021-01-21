663af7b0c28151ffb9fec08d23c481cd7f640430
Jared Manwaring <45985648+Jammasterk@users.noreply.github.com>
2021-01-21 08:32:38 -0700

    remove .env

---
+++ crossfit-attix/client/src/components/AdminForm.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+const AdminForm =  (props) => {
+    const {
+        handleChange,
+        handleSubmit,
+        btnText,
+        errMsg,
+        inputs: {
+            username, password
+        }
+    } = props
+
+    return(
+       <div>
+           
+       </div>
+    )
+}
